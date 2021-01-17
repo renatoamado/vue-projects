273999d711f070dac328400e4a56effdbaf5e0c9
Renato Amado <rsfuuton@gmail.com>
2021-01-17 20:37:51 -0300

    Jogo da Forca

---
+++ src/components/Form.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="form">
+        <div class="form-title">{{title}}</div>
+
+        <input type="text" class="form-input" v-model="inputValue">
+        <button 
+        type="button" 
+        :disabled="inputValue.length === 0"
+        @click="onSubmit"
+        >
+        {{button}}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Form',
+    props: {
+        title: String,
+        button: String,
+        action: Function
+    },
+    data() {
+        return {
+            inputValue: ''
+        }
+    },
+    methods: {
+        onSubmit: function(){
+            this.action(this.inputValue);
+            this.inputValue = ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .form{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin-top: 20px;
+    }
+
+    .form-input{
+        padding: 10px 5px;
+        border: 0;
+        border-radius: 5px;
+        margin: 10px 0;
+        text-align: center;
+        color: var(--color-text-dark);
+        font-size: 16px;
+    }
+</style>
---
+++ src/components/Teclado.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="teclado">
+      <button 
+        class="teclado-botao" 
+        v-for="(letra, key) in 'abcdefghijklmnopqrstuvwxyz'"
+        :key="key"
+        :disabled="verificarLetra(letra)"
+        @click="jogar(letra)"
+      >
+        {{letra}}
+      </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Teclado',
+    props: {
+       letras: Array,
+       verificarLetra: Function,
+       jogar: Function
+    },
+    data() {
+        return {
+         
+        }
+    },
+    methods: {
+     
+    },
+    components:{
+      
+    }
+}
+</script>
+
+<style scoped>
+    .teclado{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        max-width: 50%;
+        margin-top: 20px;
+    }
+
+    .teclado-botao{
+        margin: 5px;
+        text-transform: uppercase;
+    }
+</style>
